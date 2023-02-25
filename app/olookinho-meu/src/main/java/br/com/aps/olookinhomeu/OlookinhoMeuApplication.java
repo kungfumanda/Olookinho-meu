c7bfb110d0f86f282044eecf0f941cfd3ae42c4a
Amanda Costa <ansc@cin.ufpe.br>
2023-02-24 21:25:19 -0300

    Inicializando projeto Spring

---
+++ app/olookinho-meu/src/main/java/br/com/aps/olookinhomeu/OlookinhoMeuApplication.java
@@ -0,0 +1,13 @@
+package br.com.aps.olookinhomeu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OlookinhoMeuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OlookinhoMeuApplication.class, args);
+	}
+
+}
---
+++ app/olookinho-meu/src/test/java/br/com/aps/olookinhomeu/OlookinhoMeuApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.aps.olookinhomeu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OlookinhoMeuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
