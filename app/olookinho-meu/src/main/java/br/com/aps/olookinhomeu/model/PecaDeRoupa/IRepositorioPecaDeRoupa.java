66593d786afdd337ec50358d286241cc525535cf
Amanda Costa <ansc@cin.ufpe.br>
2023-02-24 23:06:08 -0300

    Inicializar outros arquivos

---
+++ app/olookinho-meu/src/main/java/br/com/aps/olookinhomeu/model/PecaDeRoupa/IRepositorioPecaDeRoupa.java
@@ -0,0 +1,10 @@
+package br.com.aps.olookinhomeu.model.PecaDeRoupa;
+
+import java.util.List;
+
+public interface IRepositorioPecaDeRoupa {
+   
+    void deletarPecaDeRoupa(long id)
+    void deletarPecaDeRoupa(long id);
+    void addPecaDeRoupa(PecaDeRoupa pecaDeRoupa);
+}
