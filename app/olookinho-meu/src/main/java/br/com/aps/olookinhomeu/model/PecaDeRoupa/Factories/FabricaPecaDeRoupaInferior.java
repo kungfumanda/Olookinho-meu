ec16a8dc165fb9df748479c33d2a381c4c3d6d57
Amanda Costa <ansc@cin.ufpe.br>
2023-04-08 17:58:39 -0300

    abstract factory -> factory

--- app/olookinho-meu/src/main/java/br/com/aps/olookinhomeu/model/PecaDeRoupa/Factories/FabricaPecaDeRoupaInferior.java
+++ app/olookinho-meu/src/main/java/br/com/aps/olookinhomeu/model/PecaDeRoupa/Factories/FabricaPecaDeRoupaInferior.java
@@ -3,7 +3,7 @@ package br.com.aps.olookinhomeu.model.PecaDeRoupa.Factories;
 import br.com.aps.olookinhomeu.model.PecaDeRoupa.PecaDeRoupa;
 import br.com.aps.olookinhomeu.model.PecaDeRoupa.PecaDeRoupaInferior;
 
-public class FabricaPecaDeRoupaInferior implements AbstractFactory {
+public class FabricaPecaDeRoupaInferior implements Factory {
     public PecaDeRoupa createPecaDeRoupa() {
         return new PecaDeRoupaInferior();
     }
